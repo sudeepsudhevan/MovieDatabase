b4ead3d78cba3fb4617d109d2b0b15e2ded48c00
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-26 16:05:23 +0530

    moviedatabase

---
+++ index.js
@@ -0,0 +1,134 @@
+const apiKey = "90bfdac6d036f893e82af852571bb426";
+const imgApi = "https://image.tmdb.org/t/p/w1280";
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+
+const form = document.getElementById("search-form");
+const query = document.getElementById("search-input");
+const result = document.getElementById("result");
+
+let page = 1;
+let isSearching = false;
+
+// Fetch json data from url
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Something went wrong");
+        }
+        return await response.json();
+    } catch (error) {
+        return null;
+    }
+}
+
+// Fetch movies from search based on url
+
+async function fetchAndShowResult(url) {
+    const data = await fetchData(url);
+    if (data && data.results) {
+        showResults(data.results);
+    }
+}
+
+// Create movie card html template
+
+function createMovieCard(movie) {
+    const { poster_path, original_title, release_date, overview } = movie;
+    const imagePath = poster_path ? imgApi + poster_path : "./img-01.jpeg";
+    const truncatedTile = original_title.length > 15 ? original_title.slice(0, 15) + "..." : original_title;
+    const formattedDate = release_date || "No release date";
+    const cardTemplate = `
+    <div class="column">
+        <div class="card">
+            <a class="card-media" href="./img-01.jpeg">
+                <img src="${imagePath}" alt="${original_title}"
+                width="100%"/>
+            </a>
+            <div class="card-content">
+             <div class="card-header">
+                <div class="left-content">
+                    <h3 style="font-weight: 600">${truncatedTile}</h3>
+                    <span style="color: #12efec">${formattedDate}</span>
+                </div>
+                <div class="right-content">
+                    <a href="${imagePath}" target="_blank" class="card-btn">See Cover</a>
+                </div>
+             </div>
+             <div class="info">
+                ${overview || "No overview yet..."}
+             </div>
+            </div>
+        </div>
+    </div>
+    `;
+    return cardTemplate;
+}
+
+// clear result element for new search
+
+function clearResult() {
+    result.innerHTML = "";
+}
+
+// Show results on the page
+
+function showResults(item) {
+    const newContent = item.map(createMovieCard).join("");
+    result.innerHTML += newContent || "<p>No results found</p>";
+
+}
+
+// Loard more results
+
+async function loadMoreResults() {
+    if (isSearching) {
+        return;
+    }
+    page++;
+    const searchTerm = query.value;
+    const url = searchTerm ? `${searchUrl}${searchTerm}&page=${page}` : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    await fetchAndShowResult(url);
+}
+
+// Detect end of page and load more results
+
+function detectEnd() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
+        loadMoreResults();
+    }
+}
+
+// Handle search
+
+async function handleSearch(e) {
+    e.preventDefault();
+    const searchTerm = query.value.trim();
+    if (searchTerm) {
+        isSearching = true;
+        clearResult();
+        const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
+        await fetchAndShowResult(newUrl);
+        query.value = "";
+    }
+
+}
+
+// Event listeners
+form.addEventListener("submit", handleSearch);
+window.addEventListener("scroll", detectEnd);
+window.addEventListener("resize", detectEnd);
+
+
+// initialize the page
+
+async function init() {
+    clearResult();
+    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    isSearching = false;
+    await fetchAndShowResult(url);
+}
+
+init();
